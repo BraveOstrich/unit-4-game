9a61ed9a6df22c9987a2a8c1016fa76a87047719
Steve White <stevewhitecodes@gmail.com>
2019-03-08 20:18:30 -0600

    Completed Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,13 +21,16 @@ var total4 = 0;
 
 $(document).ready(function() {
 
+    
+    
+
+
     // Create the random number
 
-    function random (){
+    function random(){
     
         randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        console.log(randomNumber);
-        $("#random-number").append(randomNumber);
+        $("#random-number").text(randomNumber);
     };
 
     // Create the crystals random number
@@ -37,7 +40,6 @@ $(document).ready(function() {
         crystalNumberTwo = Math.floor(Math.random() * 12) + 1;
         crystalNumberThree = Math.floor(Math.random() * 12) + 1;
         crystalNumberFour = Math.floor(Math.random() * 12) + 1;
-        console.log(crystalNumberOne, crystalNumberTwo, crystalNumberThree, crystalNumberFour);
     }
 
     // Crystals on click function
@@ -46,65 +48,78 @@ $(document).ready(function() {
         pointsOne++;
         total1 = crystalNumberOne * pointsOne;
         gameScore();
-        winsLosses()
-        console.log(total1);
     });
 
     $("#crystal-2").click(function(){
         pointsTwo++;
         total2 = crystalNumberTwo * pointsTwo;
         gameScore();
-        winsLosses()
-        console.log(total2);
     });
 
     $("#crystal-3").click(function(){
         pointsThree++;
         total3 = crystalNumberThree * pointsThree;
         gameScore();
-        winsLosses()
-        console.log(total3);
     });
 
     $("#crystal-4").click(function(){
         pointsFour++;
         total4 = crystalNumberFour * pointsFour;
         gameScore();
-        winsLosses()
-        console.log(total4);
     });
 
-   // Scoring function
 
-   function gameScore(){
-       
-    totalScore = total1 + total2 + total3 + total4;
-    $("#total-score").text(totalScore);
-    console.log(totalScore);
+
+    // Scoring function
+
+    function gameScore(){
+        
+        totalScore = total1 + total2 + total3 + total4;
+        $("#total-score").text(totalScore);
+        winsLosses();
     };
+    
 
     // Wins & Losses
+
     function winsLosses(){
         if(randomNumber < totalScore){
             losses++;
             $("#losses").text("Losses: " + losses);
-         } else if(totalScore === randomNumber)
-            wins++;           
+            totalScore = 0;
+            reset();
+         } else if(randomNumber == totalScore){
+            wins++;
             $("#wins").text("Wins: " + wins);
-        };
-
+            totalScore = 0;
+            reset();
+         }
+    };
+    
+    
     function reset(){
-        if(wins++ || losses++){
-            random();
-            setNumbers();
-        } 
-    }
+        randomNumber;
+        totalScore = 0;
+        $("#total-score").text(totalScore);
+        total1 = 0;
+        total2 = 0;
+        total3 = 0;
+        total4 = 0;
+        pointsOne = 0;
+        pointsTwo = 0;
+        pointsThree = 0;
+        pointsFour = 0;
+        random();
+        setNumbers();
+
+    };
+    
     // Run functions
 
-    
     random();
     setNumbers();
-    //gameScore();
+    reset();
+    
     
     
 
